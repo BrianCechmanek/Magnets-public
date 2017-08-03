be26cc5c44ca94a99e47f7c546eda954a6322c54
David Bergeron <david.f.bergeron@gmail.com>
2017-08-03 00:09:06 -0600

    Added background color

--- main.lua
+++ main.lua
@@ -1,12 +1,15 @@
 -- Magnet Game v.01
 -- Brian Cechmanek, David Bergeron
 
+COLOUR_LIGHT_BLUE = {100, 149, 237}
+
 function love.load()
   magnet = love.graphics.newImage('assets/magnet.png')
 end
 
 
 function love.draw()
+  love.graphics.clear(COLOUR_LIGHT_BLUE)
   love.graphics.draw(magnet, 100, 100)
 end
 
