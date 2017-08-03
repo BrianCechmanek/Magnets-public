0e62fae8555d6a8738f78d733e5d6d3da850886a
Brian Cechmanek <bcechmanek@gmail.com>
2017-08-03 00:24:53 -0600

    magnet unbounded movement

--- main.lua
+++ main.lua
@@ -1,6 +1,14 @@
 -- Magnet Game v.01
 -- Brian Cechmanek, David Bergeron
 
+
+
+
+mag_xpos = 32
+mag_ypos = 32
+dx = 0
+dy = 0
+
 function love.load()
   magnet = love.graphics.newImage('assets/magnet.png')
   ball = love.graphics.newImage('assets/ball.png')
@@ -8,17 +16,54 @@ end
 
 
 function love.draw()
-  love.graphics.draw(magnet, 100, 100)
+  love.graphics.draw(magnet, mag_xpos, mag_ypos)
   love.graphics.draw(ball, 600, 360)
 end
 
 
 function love.update(dt)
+  mag_xpos = mag_xpos + dx
+  mag_ypos = mag_ypos + dy
+
 end
 
 
 function love.keypressed(key)
   if key == 'q' or key == 'escape' then
     love.event.quit()
+  end
+
+  if key == 'down' then
+    dy = 32
+  end
+
+  if key == 'up' then
+    dy = -32
+  end
+
+  if key == 'left' then
+    dx = -32
+  end
+
+  if key == 'right' then
+    dx = 32
+  end
+end
+
+function love.keyreleased(key)
+  if key == 'down' then
+    dy = 0
+  end
+
+  if key == 'up' then
+    dy = 0
+  end
+
+  if key == 'left' then
+    dx = 0
+  end
+
+  if key == 'right' then
+    dx = 0
   end
 end
